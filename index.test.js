0e81cfd6244e967a51ee3e2906f160632bce8a8f
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-16 15:38:54 +0000

    add test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,59 @@
-const sum = require('./index');
+const sum = require("./index")
 
+const index = require("./index")
 describe('Sum function', () => {
   test('Sum of 4 and 6 return 10', () => {
     expect(sum(4, 6)).toEqual(10);
   });
 });
+
+// @ponicode
+describe("index", () => {
+    test("0", () => {
+        let callFunction = () => {
+            index("label_1", -10)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            index(1, "Michael")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            index(1, -1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            index(0.0, -1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            index(-10, 0)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            index(-Infinity, -Infinity)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
